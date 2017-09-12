b47b5b2c7bdadbd1bb4b368c45326c41df8c088a
project-room <31623660+project-room@users.noreply.github.com>
2017-09-12 11:05:45 +0800

    划分模块

---
+++ src/main/java/com/crm/CrmApplication.java
@@ -0,0 +1,14 @@
+package com.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.crm")
+public class CrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmApplication.class, args);
+	}
+}
